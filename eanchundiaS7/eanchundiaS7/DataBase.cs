a8955bbff1d1a88e67e79c049dcb186d2915ca8d
User <User@DESKTOP-RQTINUD>
2023-07-15 00:57:21 -0500

    Agregar archivos de proyecto.

---
+++ eanchundiaS7/eanchundiaS7/DataBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace eanchundiaS7
+{
+    public interface DataBase
+    {
+        SQLiteAsyncConnection GetConnection();  
+    }
+}
---
+++ eanchundiaS7/eanchundiaS7/Estudiante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace eanchundiaS7
+{
+    public class Estudiante
+    {
+        [PrimaryKey, AutoIncrement] //defino mi primer atributo de la tabla
+        public int Id { get; set; } // defino el nombre y tipo de atributo
+
+        [MaxLength(50)]
+        public string Nombre { get; set; }
+
+        [MaxLength(50)]
+        public string Usuario { get; set; }
+
+        [MaxLength(50)]
+        public string Contrasena { get; set; }
+
+    }
+}
